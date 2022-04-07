b3e98d9cd8fe5a6d99a35bf498d2e2c1af346b5a
Mister232 <jens.kathe@hotmail.de>
2022-04-07 23:04:30 +0200

    Some changes

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function main() {
 			adapter.log.debug('Read from textfile: ' + line);
 			
 			// Split read line to device type, address and value. Seperated by ','
-			var splitReadLine = line.split(",");
+			var splitReadLine = line.split(";");
 			
 			if (splitReadLine[0] == '3') {
 				adapter.setObjectNotExists('windowContact' + splitReadLine[1] + '.Name', {
@@ -42,7 +42,7 @@ function main() {
 					common: {name: 'windowContact' + splitReadLine[1] + '.State'},
 					native: {}
 				});
-				adapter.setObject('windowContact' + splitReadLine[1] + '.Battery', {
+				adapter.setObjectNotExists('windowContact' + splitReadLine[1] + '.Battery', {
 					type: 'string',
 					common: {name: 'windowContact' + splitReadLine[1] + '.Battery'},
 					native: {}
@@ -79,33 +79,33 @@ function main() {
 			adapter.log.debug('Read from devicelist: ' + line);
 			
 			// Split read line to device type, address and name. Seperated by ';'
-			var splitReadLine = line.split(";");
+			var splitReadLine1 = line.split(";");
 			
-			if (splitReadLine[0] == '1') {
-				adapter.setObject('socket' + splitReadLine[1] + '.Name', {
+			if (splitReadLine1[0] == '1') {
+				adapter.setObjectNotExists('socket' + splitReadLine1[1] + '.Name', {
 					type: 'string',
-					common: {name: 'socket' + splitReadLine[1] + '.Name'},
+					common: {name: 'socket' + splitReadLine1[1] + '.Name'},
 					native: {}
 				});
-				adapter.setObject('socket' + splitReadLine[1] + '.State', {
+				adapter.setObjectNotExists('socket' + splitReadLine1[1] + '.State', {
 					type: 'state',
-					common: {name: 'socket' + splitReadLine[1] + '.State'},
+					common: {name: 'socket' + splitReadLine1[1] + '.State'},
 					native: {}
 				});
-				adapter.setObject('socket' + splitReadLine[1] + '.setState', {
+				adapter.setObjectNotExists('socket' + splitReadLine1[1] + '.setState', {
 					type: 'state',
-					common: {name: 'socket' + splitReadLine[1] + '.setState'},
+					common: {name: 'socket' + splitReadLine1[1] + '.setState'},
 					native: {}
 				});
 				
-				adapter.setState('windowContact' + splitReadLine[1]  + '.Name', splitReadLine[2]);
+				adapter.setState('windowContact' + splitReadLine1[1]  + '.Name', splitReadLine1[2]);
 				
-				if (splitReadLine[3] == '0') {
-					adapter.log.debug('Socket ' + splitReadLine[2] + 'with ID ' + splitReadLine[1] + ' is switched off');
-					adapter.setState('socket' + splitReadLine[1]  + '.State', false);
-				}else if (splitReadLine[3] == '1') {
-					adapter.log.debug('Socket ' + splitReadLine[2] + 'with ID ' + splitReadLine[1] + ' is switched on');
-					adapter.setState('socket' + splitReadLine[1]  + '.State', true);
+				if (splitReadLine1[3] == '0') {
+					adapter.log.debug('Socket ' + splitReadLine1[2] + 'with ID ' + splitReadLine1[1] + ' is switched off');
+					adapter.setState('socket' + splitReadLine1[1]  + '.State', false);
+				}else if (splitReadLine1[3] == '1') {
+					adapter.log.debug('Socket ' + splitReadLine1[2] + 'with ID ' + splitReadLine1[1] + ' is switched on');
+					adapter.setState('socket' + splitReadLine1[1]  + '.State', true);
 					
 				// Check if current state = setState. If not, set new state
 				};

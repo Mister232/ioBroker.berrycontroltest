2d4144d068b4491c1eb678328c578822a041b1b5
Mister232 <jens.kathe@hotmail.de>
2022-04-07 14:04:32 +0200

    Test if slit function is working

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ adapter.on('ready', function () {
 function main() {
 
     var deviceType = adapter.config.deviceType;
-	var splitReadLine[3];
-	var testLine = '3,1,0';
 	
 	// Log output
 	adapter.log.info('Selected device type: ' + deviceType);
@@ -28,45 +26,46 @@ function main() {
 		lineReader.eachLine('/home/pi/Programs/C/BerryControl/V3.0/sensorVals.txt',(line,last)=>{
 			adapter.log.info('Read from textfile: ' + line);
 			// Split read line to device type, address and value. Seperated by ','
-			splitReadLine = testLine.split(",");;
-		})
+			let splitReadLine = line.split(",");
+			adapter.log.info('Read from textfile: ' + splitReadLine[0]);
+		});
 		
-		if (splitReadLine[0] == '3') {
-			adapter.setObject('windowContact' + splitReadLine[1] + '.State', {
-				type: 'text',
-				common: {
-					name: 'windowContact' + splitReadLine[1] + '.State',
-					type: 'text',
-					role: 'state'
-				},
-				native: {}
-			});
-			adapter.setObject('windowContact' + splitReadLine[1] + '.Battery', {
-				type: 'text',
-				common: {
-					name: 'windowContact' + splitReadLine[1] + '.Battery',
-					type: 'text',
-					role: 'state'
-				},
-				native: {}
-			});
-		};
+		// if (splitReadLine[0] == '3') {
+			// adapter.setObject('windowContact' + splitReadLine[1] + '.State', {
+				// type: 'text',
+				// common: {
+					// name: 'windowContact' + splitReadLine[1] + '.State',
+					// type: 'text',
+					// role: 'state'
+				// },
+				// native: {}
+			// });
+			// adapter.setObject('windowContact' + splitReadLine[1] + '.Battery', {
+				// type: 'text',
+				// common: {
+					// name: 'windowContact' + splitReadLine[1] + '.Battery',
+					// type: 'text',
+					// role: 'state'
+				// },
+				// native: {}
+			// });
+		// };
 		
-		if (splitReadLine[2] == '0') {
-			adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'open');
-			adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'ok');
-		}else if (splitReadLine[2] == '1') {
-			adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'close');
-			adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'ok');
-		}else if (splitReadLine[2] == '2') {
-			adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'empty');
-		}else if (splitReadLine[2] == '3') {
-			adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'open');
-			adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'empty');
-		}else if (splitReadLine[2] == '4') {
-			adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'close');
-			adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'empty');
-		};
+		// if (splitReadLine[2] == '0') {
+			// adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'open');
+			// adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'ok');
+		// }else if (splitReadLine[2] == '1') {
+			// adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'close');
+			// adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'ok');
+		// }else if (splitReadLine[2] == '2') {
+			// adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'empty');
+		// }else if (splitReadLine[2] == '3') {
+			// adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'open');
+			// adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'empty');
+		// }else if (splitReadLine[2] == '4') {
+			// adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'close');
+			// adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'empty');
+		// };
 	};
 
     // in this template all states changes inside the adapters namespace are subscribed

8e950542f3812d47c5205c8efb0f688407e18790
Mister232 <jens.kathe@hotmail.de>
2022-04-09 22:28:35 +0200

    Add command write

--- main.js
+++ main.js
@@ -128,9 +128,9 @@ function main() {
 				const fs = require('fs');
 				
 				if ('socket' + splitReadLine1[1] + '.setState' == true) {
-					fs.appendFile('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD1.txt', '1,' + splitReadLine1[1] + ',1');
+					fs.appendFile('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD1.txt', '1,1,1'); //'1,' + splitReadLine1[1] + ',1'
 				} else if ('socket' + splitReadLine1[1] + '.setState' == false) {
-					fs.appendFile('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD1.txt', '1,' + splitReadLine1[1] + ',0');
+					fs.appendFile('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD1.txt', '1,1,0');
 				};
 
 				fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/cmdAval1.txt', '1');

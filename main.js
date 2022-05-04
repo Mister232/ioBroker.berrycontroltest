2df0d3cc1786737d442092fa86548690abf45614
Jens Kathe <jens.kathe@hotmail.de>
2022-05-04 22:14:13 +0200

    Initial release after new setup

--- main.js
+++ main.js
@@ -1,158 +1,237 @@
-/**
- *
- * template adapter
- *
- *
- *  file io-package.json comments:
- *
- *  {
- *      "common": {
- *          "name":         "template",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
- *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Node.js template Adapter",  // Adapter title shown in User Interfaces
- *          "authors":  [                               // Array of authord
- *              "name <mail@template.com>"
- *          ]
- *          "desc":         "template adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
- *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
- *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
- *          "materialize":  true,                       // support of admin3
- *          "schedule":     "0 0 * * *"                 // cron-style schedule. Only needed if mode=schedule
- *          "loglevel":     "info"                      // Adapters Log Level
- *      },
- *      "native": {                                     // the native object is available via adapter.config in your adapters code - use it for configuration
- *          "test1": true,
- *          "test2": 42,
- *          "mySelect": "auto"
- *      }
- *  }
- *
- */
-
 /* jshint -W097 */// jshint strict:false
 /*jslint node: true */
 'use strict';
 
-// you have to require the utils module and call adapter function
-const utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+// Get common adapter utils
+var utils = require(__dirname + '/lib/utils'); 
 
-// you have to call the adapter function and pass a options object
-// name has to be set and has to be equal to adapters folder name and main file name excluding extension
-// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
-const adapter = new utils.Adapter('template');
 
-/*Variable declaration, since ES6 there are let to declare variables. Let has a more clearer definition where 
-it is available then var.The variable is available inside a block and it's childs, but not outside. 
-You can define the same variable name inside a child without produce a conflict with the variable of the parent block.*/
-let variable = 1234;
+var adapter = new utils.Adapter('berrycontrol');
 
-// is called when adapter shuts down - callback has to be called under any circumstances!
-adapter.on('unload', function (callback) {
-    try {
-        adapter.log.info('cleaned everything up...');
-        callback();
-    } catch (e) {
-        callback();
-    }
-});
 
-// is called if a subscribed object changes
-adapter.on('objectChange', function (id, obj) {
-    // Warning, obj can be null if it was deleted
-    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
-});
-
-// is called if a subscribed state changes
-adapter.on('stateChange', function (id, state) {
-    // Warning, state can be null if it was deleted
-    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
-
-    // you can use the ack flag to detect if it is status (true) or command (false)
-    if (state && !state.ack) {
-        adapter.log.info('ack is not set!');
-    }
-});
-
-// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
-adapter.on('message', function (obj) {
-    if (typeof obj === 'object' && obj.message) {
-        if (obj.command === 'send') {
-            // e.g. send email or pushover or whatever
-            console.log('send command');
-
-            // Send response in callback if required
-            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-        }
-    }
-});
-
-// is called when databases are connected and adapter received configuration.
-// start here!
 adapter.on('ready', function () {
     main();
 });
 
+
+adapter.on('stateChange', function (objectName, state) {
+	const fs = require('fs');
+	
+	let objectNameSplitted = objectName.split(".");
+	var deviceID = objectNameSplitted[2];
+	
+	adapter.log.info('New state for ' + objectName + ' is ' + state.val);
+	
+	if (objectName.indexOf("socket") != -1) {
+		if (state.val == 'on') {
+			fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD.txt', '1,' + deviceID.substring(6) + ',1');
+		} else if (state.val == 'off') {
+			fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD.txt', '1,' + deviceID.substring(6) + ',0');
+		};
+		
+		fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/cmdAval.txt', '1');
+		
+	}else if (objectName.indexOf("blind") != -1) {
+		// New drive value
+		if (objectName.indexOf("setState") != -1) {
+			adapter.log.info('Set state ' + state.val + '%');
+			fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD.txt', '2,' + deviceID.substring(5) + ',' + state.val);
+			
+			fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/cmdAval.txt', '1');
+		}
+		
+		// Child mode
+		else if (objectName.indexOf("childMode") != -1) {
+			if (state.val == 'on') {
+				adapter.log.info('Child mode on');
+				fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD.txt', '2,' + deviceID.substring(5) + ',CHILD_ON');
+			} else if (state.val == 'off') {
+				adapter.log.info('Child mode off');
+				fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD.txt', '2,' + deviceID.substring(5) + ',CHILD_OFF');
+			};
+			fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/cmdAval.txt', '1');
+		}
+		
+		// Stop
+		else if (objectName.indexOf("stop") != -1) {
+			if (state.val == 'true') {
+				adapter.log.info('Stop required');
+				fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/actuatorCMD.txt', '2,' + deviceID.substring(5) + ',STOP');
+				
+				fs.writeFileSync('/home/pi/Programs/C/BerryControl/V3.0/cmdAval.txt', '1');
+			};
+		};
+	};
+});
+
 function main() {
 
-    // The adapters config (in the instance object everything under the attribute "native") is accessible via
-    // adapter.config:
-    adapter.log.info('config test1: '    + adapter.config.test1);
-    adapter.log.info('config test1: '    + adapter.config.test2);
-    adapter.log.info('config mySelect: ' + adapter.config.mySelect);
+    var deviceType = adapter.config.deviceType;
+	var deviceStateList = '/home/pi/Programs/C/BerryControl/V3.0/deviceStateList.txt';
+	
+	// Log output
+	adapter.log.info('Selected device type: ' + deviceType);
 
+	// Window contact selected
+	if (deviceType == "windowContact"){
+		const lineReader = require('line-reader');
+		
+		lineReader.eachLine(deviceStateList,(line,last) => {
+			
+			// Split read line to device type, address and value. Seperated by ','
+			var splitReadLine = line.split(",");
+			
+			if (splitReadLine[0] == '3') {
+				adapter.log.info('Read from textfile: ' + line);
+				
+				adapter.setObjectNotExists('windowContact' + splitReadLine[1] + '.Name', {
+					type: 'state',
+					common: {name: 'windowContact' + splitReadLine[1] + '.Name', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('windowContact' + splitReadLine[1] + '.ID', {
+					type: 'state',
+					common: {name: 'windowContact' + splitReadLine[1] + '.ID', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('windowContact' + splitReadLine[1] + '.State', {
+					type: 'state',
+					common: {name: 'windowContact' + splitReadLine[1] + '.State', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('windowContact' + splitReadLine[1] + '.Battery', {
+					type: 'state',
+					common: {name: 'windowContact' + splitReadLine[1] + '.Battery', type: 'string'},
+					native: {}
+				});
+				
+				adapter.subscribeStates('*');
+				
+				adapter.setState('windowContact' + splitReadLine[1]  + '.Name', splitReadLine[2]);
+				adapter.setState('windowContact' + splitReadLine[1]  + '.ID', splitReadLine[1]);
+				
+				if (splitReadLine[3] == '0') {
+					adapter.log.info('Window contact ' + splitReadLine[2] + ' with ID ' + splitReadLine[1] + ' is open and battery is ok');
+					adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'open');
+					adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'ok');
+				}else if (splitReadLine[3] == '1') {
+					adapter.log.info('Window contact ' + splitReadLine[2] + ' with ID ' + splitReadLine[1] + ' is close and battery is ok');
+					adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'close');
+					adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'ok');
+				}else if (splitReadLine[3] == '2') {
+					adapter.log.info('Battery of window contact ' + splitReadLine[2] + 'with ID ' + splitReadLine[1] + ' is empty!');
+					adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'empty');
+				}else if (splitReadLine[3] == '3') {
+					adapter.log.info('Window contact ' + splitReadLine[2] + ' with ID ' + splitReadLine[1] + ' is open and battery is empty!');
+					adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'open');
+					adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'empty');
+				}else if (splitReadLine[3] == '4') {
+					adapter.log.info('Window contact ' + splitReadLine[2] + ' with ID ' + splitReadLine[1] + ' is close and battery is empty!');
+					adapter.setState('windowContact' + splitReadLine[1]  + '.State', 'close');
+					adapter.setState('windowContact' + splitReadLine[1]  + '.Battery', 'empty');
+				};
+			};
+		});
+		
+	// RF socket selected
+	} else if (deviceType == "socket"){
+		const lineReader1 = require('line-reader');
+		lineReader1.eachLine(deviceStateList,(line,last) => {
+			
+			// Split read line to device type, address and name. Seperated by ';'
+			var splitReadLine1 = line.split(",");
+			
+			if (splitReadLine1[0] == '1') {
+				adapter.log.info('Read from devicelist: ' + line);
+				
+				adapter.setObjectNotExists('socket' + splitReadLine1[1] + '.Name', {
+					type: 'state',
+					common: {name: 'socket' + splitReadLine1[1] + '.Name', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('socket' + splitReadLine1[1] + '.ID', {
+					type: 'state',
+					common: {name: 'socket' + splitReadLine1[1] + '.ID', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('socket' + splitReadLine1[1] + '.State', {
+					type: 'state',
+					common: {name: 'socket' + splitReadLine1[1] + '.State', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('socket' + splitReadLine1[1] + '.setState', {
+					type: 'state',
+					common: {name: 'socket' + splitReadLine1[1] + '.setState', type: 'string'},
+					native: {}
+				});
+				
+				adapter.subscribeStates('*');
+				
+				adapter.setState('socket' + splitReadLine1[1]  + '.Name', splitReadLine1[2]);
+				adapter.setState('socket' + splitReadLine1[1]  + '.ID', splitReadLine1[1]);
+				
+				if (splitReadLine1[3] == '0') {
+					adapter.log.info('Socket ' + splitReadLine1[2] + ' with ID ' + splitReadLine1[1] + ' is switched off');
+					adapter.setState('socket' + splitReadLine1[1]  + '.State', 'off');
+				}else if (splitReadLine1[3] == '1') {
+					adapter.log.info('Socket ' + splitReadLine1[2] + ' with ID ' + splitReadLine1[1] + ' is switched on');
+					adapter.setState('socket' + splitReadLine1[1]  + '.State', 'on');
+				};
+			};
+		});
+		
+	// RF blind control selected
+	} else if (deviceType == "blindControl"){
+		const lineReader2 = require('line-reader');
+		lineReader2.eachLine(deviceStateList,(line,last) => {
+			
+			// Split read line to device type, address and name. Seperated by ';'
+			var splitReadLine1 = line.split(",");
+			
+			if (splitReadLine1[0] == '2') {
+				adapter.log.info('Read from devicelist: ' + line);
+				
+				adapter.setObjectNotExists('blind' + splitReadLine1[1] + '.Name', {
+					type: 'state',
+					common: {name: 'blind' + splitReadLine1[1] + '.Name', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('blind' + splitReadLine1[1] + '.ID', {
+					type: 'state',
+					common: {name: 'blind' + splitReadLine1[1] + '.ID', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('blind' + splitReadLine1[1] + '.State', {
+					type: 'state',
+					common: {name: 'blind' + splitReadLine1[1] + '.State', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('blind' + splitReadLine1[1] + '.setState', {
+					type: 'state',
+					common: {name: 'blind' + splitReadLine1[1] + '.setState', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('blind' + splitReadLine1[1] + '.childMode', {
+					type: 'state',
+					common: {name: 'blind' + splitReadLine1[1] + '.setState', type: 'string'},
+					native: {}
+				});
+				adapter.setObjectNotExists('blind' + splitReadLine1[1] + '.stop', {
+					type: 'state',
+					common: {name: 'blind' + splitReadLine1[1] + '.setState', type: 'string'},
+					native: {}
+				});
+				
+				adapter.subscribeStates('*');
+				
+				adapter.setState('blind' + splitReadLine1[1]  + '.Name', splitReadLine1[2]);
+				adapter.setState('blind' + splitReadLine1[1]  + '.ID', splitReadLine1[1]);
+				adapter.setState('blind' + splitReadLine1[1]  + '.childMode', 'off');
+				adapter.setState('blind' + splitReadLine1[1]  + '.ID', 'false');
+				adapter.setState('blind' + splitReadLine1[1]  + '.State', splitReadLine1[3]);
 
-    /**
-     *
-     *      For every state in the system there has to be also an object of type state
-     *
-     *      Here a simple template for a boolean variable named "testVariable"
-     *
-     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-     *
-     */
-
-    adapter.setObject('testVariable', {
-        type: 'state',
-        common: {
-            name: 'testVariable',
-            type: 'boolean',
-            role: 'indicator'
-        },
-        native: {}
-    });
-
-    // in this template all states changes inside the adapters namespace are subscribed
-    adapter.subscribeStates('*');
-
-
-    /**
-     *   setState examples
-     *
-     *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-     *
-     */
-
-    // the variable testVariable is set to true as command (ack=false)
-    adapter.setState('testVariable', true);
-
-    // same thing, but the value is flagged "ack"
-    // ack should be always set to true if the value is received from or acknowledged from the target system
-    adapter.setState('testVariable', {val: true, ack: true});
-
-    // same thing, but the state is deleted after 30s (getState will return null afterwards)
-    adapter.setState('testVariable', {val: true, ack: true, expire: 30});
-
-
-
-    // examples for the checkPassword/checkGroup functions
-    adapter.checkPassword('admin', 'iobroker', function (res) {
-        console.log('check user admin pw ioboker: ' + res);
-    });
-
-    adapter.checkGroup('admin', 'admin', function (res) {
-        console.log('check group user admin group admin: ' + res);
-    });
-
-
-
+				adapter.log.info('Blind ' + splitReadLine1[2] + ' with ID ' + splitReadLine1[1] + ' and current state ' + splitReadLine1[3] + '%');
+			};
+		});
+	};
 }
